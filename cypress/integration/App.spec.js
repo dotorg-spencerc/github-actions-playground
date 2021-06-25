e2643913f487df14a1787c7671fdf61585189744
Spencer Carney <spencerc@dotorg.io>
2021-06-25 15:09:46 -0700

    fixing failing test

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -2,7 +2,6 @@ describe('App', () => {
   it('shows paragraph tag', () => {
     cy.visit('/');
     cy.getBySel('p').should('be.visible');
-    cy.getBySel('foo').should('be.visible');
   });
 });
 
